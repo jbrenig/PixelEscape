0bcb4ca6919e4d1947fb62fee1ef7e75af349c62
Jonas Brenig <jbrenig@uni-bremen.de>
2017-10-14 01:09:58 +0200

    finish porting game.data package to kotlin

---
+++ core/src/net/brenig/pixelescape/game/data/GameMode.kt
@@ -0,0 +1,315 @@
+package net.brenig.pixelescape.game.data
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
+import net.brenig.pixelescape.game.World
+import net.brenig.pixelescape.game.data.constants.StyleNames
+import net.brenig.pixelescape.game.data.constants.Textures
+import net.brenig.pixelescape.game.player.Item
+import net.brenig.pixelescape.game.player.abliity.Abilities
+import net.brenig.pixelescape.game.player.abliity.Ability
+import net.brenig.pixelescape.game.player.effects.EffectMove
+import net.brenig.pixelescape.game.player.item.ItemLife
+import net.brenig.pixelescape.game.player.item.ItemScoreDynamic
+import net.brenig.pixelescape.game.player.movement.DefaultMovementController
+import net.brenig.pixelescape.game.player.movement.DragMovementController
+import net.brenig.pixelescape.game.player.movement.FlashMovementController
+import net.brenig.pixelescape.game.player.movement.PlayerMovementController
+import net.brenig.pixelescape.game.worldgen.WorldGenerator
+import net.brenig.pixelescape.game.worldgen.special.BarricadeGenerator
+import net.brenig.pixelescape.game.worldgen.special.ItemGenerator
+import net.brenig.pixelescape.lib.FilteredElementProvider
+import net.brenig.pixelescape.lib.Names
+import net.brenig.pixelescape.lib.Reference
+import net.brenig.pixelescape.render.ui.general.SwipeTabbedStack
+
+/**
+ * GameMode information
+ */
+enum class GameMode {
+    CLASSIC("Classic", Names.SCOREBOARD_CLASSIC, "gamemode_classic") {
+        override fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {
+            val maxLabelWidth = contentSizeX - 60
+            val table = Table(skin)
+            table.setBackground(Textures.BUTTON_UP)
+            table.defaults().padBottom(20f)
+            run {
+                val lbl = Label("Good old classic!", skin)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            stack.add(table)
+        }
+    },
+    ARCADE("Arcade", Names.SCOREBOARD_ARCADE, "gamemode_arcade", true, extraLives = 2) {
+        override fun registerWorldGenerators(worldGenerator: WorldGenerator) {
+            super.registerWorldGenerators(worldGenerator)
+            worldGenerator.addSpecialGenerator(ItemGenerator(600, 1000, 800, 1600, ItemGenerator.createDefaultItemList()))
+            worldGenerator.addSpecialGenerator(ItemGenerator(20000, 25000, 30000, 35000, FilteredElementProvider.SingleElementProvider<Item>(ItemLife.ITEM)))
+            worldGenerator.addSpecialGenerator(ItemGenerator(25000, 30000, 50000, 60000, FilteredElementProvider.SingleElementProvider(EffectMove.ITEM)))
+            worldGenerator.addSpecialGenerator(ItemGenerator(25000, 50000, 60000, 70000, FilteredElementProvider.SingleElementProvider<Item>(ItemScoreDynamic.ITEM)))
+        }
+
+        override fun itemsEnabled(): Boolean {
+            return true
+        }
+
+        override fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {
+            val maxLabelWidth = contentSizeX - 60
+            val table = Table(skin)
+            table.setBackground(Textures.BUTTON_UP)
+            table.defaults().padBottom(20f)
+            run {
+                val lbl = Label("Like Classic, but cooler!", skin, StyleNames.LABEL_WHITE)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.color = Color.PURPLE
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            stack.add(table)
+        }
+    },
+    SPEED("Speed", Names.SCOREBOARD_SPEED, "gamemode_speed",
+            maxEntitySpeed = Reference.MAX_ENTITY_SPEED * 1.2f,
+            startingSpeed = Reference.STARTING_SPEED * 2,
+            speedIncreaseFactor = Reference.SPEED_MODIFIER * 3) {
+        override fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {
+            val maxLabelWidth = contentSizeX - 60
+            val table = Table(skin)
+            table.setBackground(Textures.BUTTON_UP)
+            table.defaults().padBottom(20f)
+            run {
+                val lbl = Label("Watch out! It will get fast!", skin, StyleNames.LABEL_WHITE)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.color = Color.ORANGE
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            stack.add(table)
+        }
+    },
+    FLASH("Flash", Names.SCOREBOARD_FLASH, "gamemode_flash",
+            maxEntitySpeed = Reference.MAX_ENTITY_SPEED * 1.4f,
+            startingSpeed = Reference.STARTING_SPEED * 4,
+            speedIncreaseFactor = Reference.SPEED_MODIFIER * 4) {
+        override fun registerWorldGenerators(worldGenerator: WorldGenerator) {
+            worldGenerator.registerDefaultTerrainGenerators()
+            worldGenerator.addSpecialGenerator(BarricadeGenerator((Reference.TARGET_RESOLUTION_X * 0.7f).toInt()))
+        }
+
+        override fun createPlayerMovementController(): PlayerMovementController {
+            return FlashMovementController()
+        }
+
+        override fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {
+            val maxLabelWidth = contentSizeX - 60
+            val table = Table(skin)
+            table.setBackground(Textures.BUTTON_UP)
+            table.defaults().padBottom(20f)
+            run {
+                val lbl = Label("Ready for a challenge?", skin, StyleNames.LABEL_WHITE)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.color = Color.NAVY
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            table.row()
+            run {
+                val lbl = Label("You'll need to react quickly!", skin, StyleNames.LABEL_WHITE)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.color = Color.NAVY
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            stack.add(table)
+        }
+    },
+    BLINK("Blink", Names.SCOREBOARD_BLINK, "gamemode_blink", true, Abilities.BLINK, -1) {
+        override fun registerWorldGenerators(worldGenerator: WorldGenerator) {
+            worldGenerator.registerDefaultTerrainGenerators()
+            worldGenerator.addSpecialGenerator(BarricadeGenerator(400))
+            worldGenerator.obstacleSizeModifier = 1.5f
+        }
+
+        override fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {
+            val maxLabelWidth = contentSizeX - 60
+            val table = Table(skin)
+            table.setBackground(Textures.BUTTON_UP)
+            table.defaults().padBottom(20f)
+            run {
+                val lbl = Label("Like teleporting? Here you go!", skin, StyleNames.LABEL_WHITE)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.color = Color.NAVY
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            stack.add(table)
+        }
+    },
+    DRAG("Drag", Names.SCOREBOARD_DRAG, "gamemode_drag") {
+        override fun createPlayerMovementController(): PlayerMovementController {
+            return DragMovementController()
+        }
+
+        override fun registerWorldGenerators(worldGenerator: WorldGenerator) {
+            super.registerWorldGenerators(worldGenerator)
+            worldGenerator.obstacleSizeModifier = 0.7f
+        }
+
+        override fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {
+            val maxLabelWidth = contentSizeX - 60
+            val table = Table(skin)
+            table.setBackground(Textures.BUTTON_UP)
+            table.defaults().padBottom(20f)
+            run {
+                val lbl = Label("Just drag along!", skin, StyleNames.LABEL_WHITE)
+                lbl.setWrap(true)
+                lbl.setAlignment(Align.center)
+                lbl.color = Color.FIREBRICK
+                lbl.pack()
+
+                table.add(lbl).width(maxLabelWidth.toFloat()).center()
+            }
+            stack.add(table)
+        }
+    };
+
+    /**
+     * @return the name of this gamemode (unlocalized)
+     */
+    val gameModeName: String
+
+    /**
+     * @return the name of the scoreboard that should be used
+     */
+    val scoreboardName: String
+
+    private val iconTexture: String
+
+    private val abilitiesEnabled: Boolean
+    /**
+     * @return the ability that is available when the game begins (returns null if abilities are disabled)
+     */
+    val startingAbility: Ability?
+
+    /**
+     * @return the uses the starting ability has left (default -1)
+     */
+    val startingAbilityUses: Int
+    /**
+     * @return the amount of extra lives the player has when the game starts
+     */
+    val extraLives: Int
+
+    /**
+     * @return maximum speed of entities
+     */
+    val maxEntitySpeed: Float
+    /**
+     * @return the speed the player has when the game starts
+     */
+    val startingSpeed: Float
+    /**
+     * @return the speed increase of the player
+     */
+    val speedIncreaseFactor: Float
+
+    constructor(name: String,
+                        scoreBoardName: String,
+                        iconTexture: String,
+                        abilitiesEnabled: Boolean = false,
+                        startingAbility: Ability? = null,
+                        startingAbilityUses: Int = -1,
+                        extraLives: Int = 0,
+                        maxEntitySpeed: Float = Reference.MAX_ENTITY_SPEED,
+                        startingSpeed: Float = Reference.STARTING_SPEED,
+                        speedIncreaseFactor: Float = Reference.SPEED_MODIFIER) {
+        this.gameModeName = name
+        this.scoreboardName = scoreBoardName
+        this.iconTexture = iconTexture
+        this.abilitiesEnabled = abilitiesEnabled
+        this.startingAbility = startingAbility
+        this.startingAbilityUses = startingAbilityUses
+        this.extraLives = extraLives
+        this.maxEntitySpeed = maxEntitySpeed
+        this.startingSpeed = startingSpeed
+        this.speedIncreaseFactor = speedIncreaseFactor
+    }
+
+
+    /**
+     * @return the movement controller used to do the players movement
+     */
+    open fun createPlayerMovementController(): PlayerMovementController {
+        return DefaultMovementController()
+    }
+
+    /**
+     * finds the texture region for the gamemode icon
+     *
+     * @return the icon of this gamemode
+     */
+    fun createIcon(assets: GameAssets): TextureRegion {
+        return assets.textureAtlas.findRegion(iconTexture)
+    }
+
+    /**
+     * add WorldFeatures to the given worldGenerator
+     */
+    open fun registerWorldGenerators(worldGenerator: WorldGenerator) {
+        worldGenerator.registerDefaultWorldGenerators()
+    }
+
+    /**
+     * gets called every tick
+     *
+     * @param world the world to generate in
+     * @return whether barricades should be generated
+     */
+    fun shouldGenerateBarricades(world: World): Boolean {
+        return true
+    }
+
+    /**
+     * whether abilities are enabled (note: this value must not change!)
+     */
+    fun abilitiesEnabled(): Boolean {
+        return abilitiesEnabled
+    }
+
+    /**
+     * @return whether items are enabled (used for tutorial)
+     */
+    open fun itemsEnabled(): Boolean {
+        return false
+    }
+
+    /**
+     * ability for gamemodes to add custom tutorial pages
+     *
+     * @param skin         skin used for ui
+     * @param stack        stack the pages should get added to
+     * @param contentSizeX target X size for the elements added
+     * @param contentSizeY target Y size for the elements added
+     */
+    open fun createCustomTutorial(skin: Skin, stack: SwipeTabbedStack, contentSizeX: Int, contentSizeY: Int) {}
+
+
+}
---
+++ core/src/net/brenig/pixelescape/game/data/GameMusic.kt
@@ -0,0 +1,198 @@
+package net.brenig.pixelescape.game.data
+
+import com.badlogic.gdx.audio.Music
+import net.brenig.pixelescape.PixelEscape
+import net.brenig.pixelescape.lib.Reference
+import net.brenig.pixelescape.lib.Utils
+
+/**
+ * Music wrapper with additional features such as fading and queueing
+ */
+class GameMusic(private val game: PixelEscape) {
+
+    private var currentMusic: Music? = null
+
+    /**
+     * The target Music that should get faded in
+     */
+    private var fadeInMusic: Music? = null
+    private var fadingProgress = 0f
+    private var fadingTime = 0f
+
+    private var currentVolume = 1f
+
+    private var state = MusicState.STOPPED
+
+    /**
+     * @return true if fading is in progress
+     */
+    val isFading: Boolean
+        get() = state == MusicState.FADE_IN || state == MusicState.FADE_OUT_STOP || state == MusicState.FADE_OUT_PAUSE || state == MusicState.FADE_OUT_INTO
+
+    val isPlaying: Boolean
+        get() = state != MusicState.STOPPED && state != MusicState.PAUSED
+
+    fun getState() = state.toString();
+
+    fun setCurrentMusic(m: Music?) {
+        stop()
+        if (Reference.ENABLE_MUSIC) {
+            currentMusic = m
+            if (currentMusic != null) {
+                currentMusic!!.volume = currentVolume
+            }
+        }
+    }
+
+    /**
+     * this function has to be called for fading
+     *
+     * @param delta time passed
+     */
+    fun update(delta: Float) {
+        if (Reference.ENABLE_MUSIC && isFading) {
+            fadingProgress += delta
+            if (state == MusicState.FADE_IN) {
+                currentVolume = Utils.easeInAndOut(fadingProgress, fadingTime) * game.gameSettings.musicVolume
+                if (fadingProgress > fadingTime) {
+                    state = MusicState.PLAYING
+                }
+            } else {
+                currentVolume = game.gameSettings.musicVolume - Utils.easeInAndOut(fadingProgress, fadingTime) * game.gameSettings.musicVolume
+                if (fadingProgress > fadingTime) {
+                    fadingProgress = 0f
+                    if (state == MusicState.FADE_OUT_PAUSE) {
+                        pause()
+                    } else if (state == MusicState.FADE_OUT_INTO) {
+                        setFadeInMusicCurrent()
+                        fadeIn()
+                    } else {
+                        stop()
+                    }
+                }
+            }
+            currentMusic!!.volume = currentVolume
+        }
+    }
+
+    fun updateMusicVolume() {
+        if (!isFading) {
+            currentVolume = game.gameSettings.musicVolume
+            currentMusic!!.volume = game.gameSettings.musicVolume
+        }
+    }
+
+    fun fadeIn() {
+        play(true)
+    }
+
+    /**
+     * stops current music if possible
+     */
+    fun stop() {
+        if (currentMusic != null && state != MusicState.STOPPED) {
+            currentMusic!!.stop()
+            state = MusicState.STOPPED
+            setFadeInMusicCurrent()
+        }
+    }
+
+    /**
+     * pauses current music if possible
+     */
+    fun pause() {
+        if (currentMusic != null && currentMusic!!.isPlaying) {
+            currentMusic!!.pause()
+            state = MusicState.PAUSED
+            setFadeInMusicCurrent()
+        }
+    }
+
+    /**
+     * sets music to fade into as current music (and stops any music playing)
+     */
+    fun setFadeInMusicCurrent() {
+        if (fadeInMusic != null) {
+            setCurrentMusic(fadeInMusic)
+            fadeInMusic = null
+        }
+    }
+
+    fun play() {
+        play(false, 0f)
+    }
+
+    @JvmOverloads
+    fun play(fadeIn: Boolean, fadeInTime: Float = defaultFadingTime) {
+        if (Reference.ENABLE_MUSIC) {
+            if (currentMusic != null && state != MusicState.PLAYING && game.gameSettings.isMusicEnabled) {
+                if (fadeIn) {
+                    if (!isFading) {
+                        fadingProgress = 0f
+                        currentMusic!!.volume = 0f
+                    }
+                    fadingTime = fadeInTime
+                    state = MusicState.FADE_IN
+                } else {
+                    currentMusic!!.volume = game.gameSettings.musicVolume
+                    fadingProgress = 0f
+                    fadingTime = 0f
+                    state = MusicState.PLAYING
+                }
+                currentMusic!!.play()
+            }
+        }
+    }
+
+    fun playOrFadeInto(music: Music?) {
+        if (Reference.ENABLE_MUSIC) {
+            if (music == null) {
+                stop()
+            } else if (currentMusic != null && isPlaying) {
+                if (currentMusic === music) {
+                    fadeIn()
+                    return
+                }
+                fadeIntoMusic(music)
+            } else {
+                setCurrentMusic(music)
+                play()
+            }
+        }
+    }
+
+    @JvmOverloads
+    fun fadeOutToStop(time: Float = defaultFadingTime) {
+        if (!isFading || state == MusicState.FADE_IN) {
+            fadingProgress = 0f
+            fadingTime = time
+        }
+        state = MusicState.FADE_OUT_STOP
+    }
+
+    @JvmOverloads
+    fun fadeOutToPause(time: Float = defaultFadingTime) {
+        fadingProgress = 0f
+        fadingTime = time
+        state = MusicState.FADE_OUT_PAUSE
+    }
+
+    /**
+     * Fades current music out, and fades given music in
+     *
+     * @param music music to play next
+     */
+    fun fadeIntoMusic(music: Music) {
+        fadeInMusic = music
+        state = MusicState.FADE_OUT_INTO
+    }
+
+    private enum class MusicState {
+        PLAYING, PAUSED, STOPPED, FADE_IN, FADE_OUT_STOP, FADE_OUT_PAUSE, FADE_OUT_INTO
+    }
+
+    companion object {
+        private const val defaultFadingTime = 1f
+    }
+
+}

46fc5bd5c503ba5b0e44ba7a12e7a939c0efca15
Jonas Brenig <jbrenig@uni-bremen.de>
2024-09-15 22:06:03 +0200

    update android

--- android/src/main/kotlin/net/brenig/pixelescape/android/AndroidLauncher.kt
+++ android/src/main/kotlin/net/brenig/pixelescape/android/AndroidLauncher.kt
@@ -3,29 +3,32 @@ package net.brenig.pixelescape.android
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
-import de.golfgl.gdxgamesvcs.GpgsClient
 import net.brenig.pixelescape.PixelEscape
 import net.brenig.pixelescape.game.data.GameConfiguration
+import de.golfgl.gdxgamesvcs.GpgsClient
 
+/** Launches the Android application. */
 class AndroidLauncher : AndroidApplication() {
     lateinit var configuration : AndroidConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val config = AndroidApplicationConfiguration()
+        config.useImmersiveMode = true // Recommended, but not required.
         config.useAccelerometer = false
         config.useCompass = false
         configuration = AndroidConfiguration(this)
         initialize(PixelEscape(configuration), config)
     }
 
-    class AndroidConfiguration(val activity: Activity) : GameConfiguration() {
+    class AndroidConfiguration(val activity: AndroidApplication) : GameConfiguration() {
         override val gameService by lazy {
             GpgsClient()
-                    .setGpgsAchievementIdMapper(PlayServicesMapper.Companion::mapAchievement)
-                    .setGpgsLeaderboardIdMapper(PlayServicesMapper.Companion::mapLeaderboard)!!
+                .setGpgsAchievementIdMapper(PlayServicesMapper.Companion::mapAchievement)
+                .setGpgsLeaderboardIdMapper(PlayServicesMapper.Companion::mapLeaderboard)!!
         }
         override val gameServiceAvailable = true
         override val canQuitGame = false
@@ -38,7 +41,7 @@ class AndroidLauncher : AndroidApplication() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        configuration.gameService.onGpgsActivityResult(requestCode, resultCode, data)
+        configuration.gameService.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
     }
 }

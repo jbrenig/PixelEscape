0a4d01a56963acaf895f1071e4dbc5e2ae2d72ad
Jonas Brenig <jbrenig@uni-bremen.de>
2024-09-15 22:34:34 +0200

    readd html (not working)

---
+++ teavm/src/main/kotlin/net/brenig/pixelescape/teavm/TeaVMLauncher.kt
@@ -0,0 +1,23 @@
+@file:JvmName("TeaVMLauncher")
+
+package net.brenig.pixelescape.teavm
+
+import com.github.xpenatan.gdx.backends.teavm.TeaApplicationConfiguration
+import com.github.xpenatan.gdx.backends.teavm.TeaApplication
+import net.brenig.pixelescape.PixelEscape
+
+/** Launches the TeaVM/HTML application. */
+fun main() {
+    val config = TeaApplicationConfiguration("canvas").apply {
+        //// If width and height are each greater than 0, then the app will use a fixed size.
+        //width = 640
+        //height = 480
+        //// If width and height are both 0, then the app will use all available space.
+        //width = 0
+        //height = 0
+        //// If width and height are both -1, then the app will fill the canvas size.
+        width = -1
+        height = -1
+    }
+    TeaApplication(PixelEscape(), config)
+}
